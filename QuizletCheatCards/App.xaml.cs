708b0e043ecfe16a308bc5c91b1015ac47ec0a05
Isaac Duarte <31712685+Isaac-Duarte@users.noreply.github.com>
2020-01-21 13:01:08 -0700

    MVVM Base

---
+++ QuizletCheatCards/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using QuizletCheatCards.View;
+using QuizletCheatCards.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuizletCheatCards
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainView view = new MainView();
+            view.DataContext = new MainViewModel();
+            view.Show();
+        }
+    }
+}
---
+++ QuizletCheatCards/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizletCheatCards.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
